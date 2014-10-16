5b398d78d0870a08b638f7676ed913cb5e79bcf4
Jongwook Choi <wookayin@gmail.com>
2014-10-16 14:34:21 +0900

    Basic implementation of acessing (reading) user memory
    To retrieve the system call number, kernel should examine the stack
    area of user process.
    
    For the sake of it, functions `get_user` (reading a single byte) and
    `memread_user` (reading a consecutive block of user memory) are
    implemented. We also verify if a user pointer is valid area,
    which would be the case the user program should be terminated.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,12 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+static int memread_user (void *src, void *des, size_t bytes);
+
 
 void
 syscall_init (void)
@@ -13,8 +17,50 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED)
+syscall_handler (struct intr_frame *f)
 {
-  printf ("system call!\n");
+  int syscall_number;
+  ASSERT( sizeof(syscall_number) == 4 ); // assuming x86
+
+  // The system call number is in the 32-bit word at the caller's stack pointer.
+  if (memread_user(f->esp, &syscall_number, sizeof(syscall_number)) == -1) {
+    thread_exit (); // invalid memory access, terminate the user process
+    return;
+  }
+
+  printf ("[DEBUG] system call, number = %d!\n", syscall_number);
   thread_exit ();
+}
+
+/****************** Helper Functions on Memory Access ********************/
+
+static int32_t
+get_user (const uint8_t *uaddr) {
+  // check that a user pointer `uaddr` points below PHYS_BASE
+  if (! ((void*)uaddr < PHYS_BASE))
+    return -1; // invalid memory access
+
+  // as suggested in the reference manual, see (3.1.5)
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+      : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/**
+ * Reads a consecutive `bytes` bytes of user memory with the
+ * starting address `src` (uaddr), and writes to dst.
+ * Returns the number of bytes read, or -1 on page fault (invalid memory access)
+ */
+static int
+memread_user (void *src, void *dst, size_t bytes)
+{
+  int32_t value;
+  size_t i;
+  for(i=0; i<bytes; i++) {
+    value = get_user(src + i);
+    if(value < 0) return -1; // invalid memory access.
+    *(char*)(dst + i) = value & 0xff;
+  }
+  return (int)bytes;
 }
