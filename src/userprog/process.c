03e2bc00bea3111575619d41e2cc20ef3c28ee23
Jongwook Choi <wookayin@gmail.com>
2014-10-16 14:31:56 +0900

    Temporary fix for esp in setup_stack
    Argument passing is not yet implemented, hence the workaround.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -444,7 +444,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;  // temporary fix, as suggested IN THE MANUAL
       else
         palloc_free_page (kpage);
     }
