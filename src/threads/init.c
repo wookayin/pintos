b20fe2c9e2d132f5b6e4f03e80bfa8d492b088b7
Jongwook Choi <wookayin@gmail.com>
2014-11-14 16:41:11 +0900

    Implement a basic swap table
    Swap table is implemented: see vm_swap_in() and vm_swap_out().
    Each page is assigned to contiguous sectors of total size PGSIZE,
    to store the content of a page being swapped out.

--- src/threads/init.c
+++ src/threads/init.c
@@ -33,6 +33,7 @@
 #endif
 #ifdef VM
 #include "vm/frame.h"
+#include "vm/swap.h"
 #endif
 #ifdef FILESYS
 #include "devices/block.h"
@@ -133,6 +134,9 @@ main (void)
   ide_init ();
   locate_block_devices ();
   filesys_init (format_filesys);
+#endif
+#ifdef VM
+  vm_swap_init ();
 #endif
 
   printf ("Boot complete.\n");
